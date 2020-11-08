53eb64d8b44515c62d65177655c3cabb2965d93c
catdevdev <catdev.dev@gmail.com>
2020-11-08 23:20:21 +0200

    finish implemention unity webgl (Hooray!!!!!!!!)

--- pages/game.js
+++ pages/game.js
@@ -21,7 +21,10 @@ const Text = styled.div``
 /* unity webgl */
 import dynamic from 'next/dynamic'
 
+// import Unity, {  } from 'react-unity-webgl'
 import Unity, { UnityContext } from 'react-unity-webgl'
+// import Unity, { UnityContent } from 'react-unity-webgl'
+
 // const
 //   Unity= dynamic(() => import('react-unity-webgl'), { ssr: false })
 
@@ -30,33 +33,18 @@ import Unity, { UnityContext } from 'react-unity-webgl'
 //   'test-build-for-react/Build/UnityLoader.js',
 // )
 
-// const unityContext = new UnityContext({
-//   loaderUrl: 'build/Build/UnityLoader.js',
-//   dataUrl: 'build/Build/build.unityweb',
-//   frameworkUrl: 'build/Build/build.framework.unityweb',
-//   codeUrl: 'build/Build/build.wasm',
-//   // streamingAssetsUrl: 'build/streamingAssets/',
-// })
-
-// this.unityContext = new UnityContext({
-//   loaderUrl: "build/myunityapp.loader.js",
-//   dataUrl: "build/myunityapp.data",
-//   frameworkUrl: "build/myunityapp.framework.js",
-//   codeUrl: "build/myunityapp.wasm"
-// });
-
-const unityContent = new UnityContext({
-  loaderUrl: "build/myunityapp.loader.js",
-  dataUrl: "build/myunityapp.data",
-  frameworkUrl: "build/myunityapp.framework.js",
-  codeUrl: "build/myunityapp.wasm",
-})
-
 const Game = () => {
+  const unityContext = new UnityContext({
+    dataUrl: 'test6/Build/test6.data',
+    frameworkUrl: 'test6/Build/test6.framework.js',
+    loaderUrl: 'test6/Build/test6.loader.js',
+    codeUrl: 'test6/Build/test6.wasm',
+  })
+
   return (
     <Background>
       <Text>Game</Text>
-      <Unity unityContext={unityContent}></Unity>
+      <Unity unityContext={unityContext}></Unity>
     </Background>
   )
 }
