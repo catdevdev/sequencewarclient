41380a39b3af3017e5e2eb81db6c0fb52b03667a
catdevdev <catdev.dev@gmail.com>
2020-11-11 22:48:16 +0200

    typo

--- redux/reducers/modals.js
+++ redux/reducers/modals.js
@@ -8,44 +8,44 @@ import {
   SHOW_MODAL_SETTINGS_ROOM,
   HIDE_MODAL_SETTINGS_ROOM,
   SHOW_MODAL_ALERT,
-  HIDE_MODAL_ALERT
-} from '../actiontypes';
+  HIDE_MODAL_ALERT,
+} from '../actiontypes'
 
 const initialState = {
   showInputUser: false,
-  showMobilechat: false,
+  showMobileChat: false,
   showSettingsSpaceship: false,
   showSettingsRoom: false,
   alert: { show: false, text: null },
-};
+}
 
 export const modalsReducer = (state = initialState, action) => {
   switch (action.type) {
     case SHOW_MODAL_INPUT_USER:
-      return { ...state, showInputUser: true };
+      return { ...state, showInputUser: true }
     case HIDE_MODAL_INPUT_USER:
-      return { ...state, showInputUser: false };
+      return { ...state, showInputUser: false }
 
     case SHOW_MODAL_MOBILE_CHAT:
-      return { ...state, showMobilechat: true };
+      return { ...state, showMobilechat: true }
     case HIDE_MODAL_MOBILE_CHAT:
-      return { ...state, showMobilechat: false };
+      return { ...state, showMobilechat: false }
 
     case SHOW_MODAL_SETTINGS_SPACESHIP:
-      return { ...state, showSettingsSpaceship: true };
+      return { ...state, showSettingsSpaceship: true }
     case HIDE_MODAL_SETTINGS_SPACESHIP:
-      return { ...state, showSettingsSpaceship: false };
+      return { ...state, showSettingsSpaceship: false }
 
     case SHOW_MODAL_SETTINGS_ROOM:
-      return { ...state, showSettingsRoom: true };
+      return { ...state, showSettingsRoom: true }
     case HIDE_MODAL_SETTINGS_ROOM:
-      return { ...state, showSettingsRoom: false };
+      return { ...state, showSettingsRoom: false }
     case SHOW_MODAL_ALERT:
-      return { ...state, alert: { show: true, text: action.payload } };
+      return { ...state, alert: { show: true, text: action.payload } }
     case HIDE_MODAL_ALERT:
-      return { ...state, alert: { show: false, text: null } };
+      return { ...state, alert: { show: false, text: null } }
 
     default:
-      return state;
+      return state
   }
-};
+}
